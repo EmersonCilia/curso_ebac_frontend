b8edc5c4b00361bc93295d4a7aef6f707ecab8b2
Emerson Willian <emersonwpprado@gmail.com>
2024-08-07 11:52:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ document.getElementById('numberForm').addEventListener('submit', function(event)
     errorDiv.hidden = true;
 
     switch (true) {
-        case num1 > num2:
-            resultDiv.innerText = `O primeiro número (${num1}) é maior que o segundo número (${num2}).`;
+        case num2 > num1:
+            resultDiv.innerText = `O primeiro número (${num2}) é maior que o segundo número (${num1}).`;
             resultDiv.hidden = false;
             break;
-        case num1 < num2:
+        case num2 < num1:
             errorDiv.innerText = `O segundo número (${num2}) é maior que o primeiro número (${num1}), o primero número deve ser maior que o segundo.`;
             errorDiv.hidden = false;
             break;
